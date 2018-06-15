ed01d9fbf19478611ab45f2904b6513855cddd39
John Dupuy <jdupuy26@gmail.com>
2018-06-15 16:38:09 -0400

    Add system arguments and new structure

--- pseudo_spectral.py
+++ pseudo_spectral.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 import numpy as np
 import matplotlib.pyplot as plt
+# Scipy imports
 from scipy.special import kn 
 from scipy.integrate import trapz
-
+# Argparse
+import argparse
+from argparse import RawTextHelpFormatter
 
 #===================================================================
 #
@@ -78,9 +81,14 @@ def Ifunc(Gfunc, r, rp, pmpp):
 
 # \func init_data()
 # Initialize the density data based on test problem  
-def init_data(prob,nx1,nx2): 
+def init_data(prob,R,p): 
+    nx1, nx2 = len(R), len(p) 
     if prob == 'constant':
         data = np.ones( (nx2, nx1) ) 
+    elif prob == 'exp':
+        R, p = np.meshgrid(R,p) 
+        sig  = 1.
+        data = np.exp(-R/sig)/(2.*np.pi*sig**2.)
     else:
         print('[init_data]: Prob not understood, got %s, exiting...' %(prob))
         quit() 
@@ -89,8 +97,7 @@ def init_data(prob,nx1,nx2):
 # \func init_grid()
 # Initialize the 2D polar grid 
 # x1: R, x2: \phi 
-def init_grid(x1min,x1max,nx1,nx2,x2min=0.0,x2max=2.0*np.pi,log=False): 
-
+def init_grid(x1min,x1max,x2min,x2max,nx1,nx2,log=False): 
     # First get the face-centered grid  
     if log: # for a log grid 
         lx1f = np.linspace(np.log(x1min),
@@ -101,16 +108,22 @@ def init_grid(x1min,x1max,nx1,nx2,x2min=0.0,x2max=2.0*np.pi,log=False):
                            x1max, nx1+1)
     x2f = np.linspace(x2min,
                       x2max, nx2+1) 
-    # Now get the cell-centered grid
-    x1c = 0.5*(x1f[1:] + x1f[:-1])
-    x2c = 0.5*(x2f[1:] + x2f[:-1]) 
-    # So that x1c and x1f are the same shape
-    # exclude the last point 
-    x1f = x1f[:-1]
-    x2f = x2f[:-1] 
+
+    # Now get the cell-centered grid, 2 ghost zones, one on left and one on right 
+    if log:
+        dlx1 = lx1f[1]-lx1f[0]
+        lx1c = np.arange(np.log(x1min)-0.5*dlx1,
+                         np.log(x1max)+    dlx1, dlx1)
+        x1c  = np.exp(lx1c)  
+    else:
+        dx1 = x1f[1] - x1f[0]
+        x1c = np.arange(x1min-0.5*dx1,
+                        x1max+    dx1, dx1)   
+    
+    dx2 = x2f[1] - x2f[0] 
+    x2c = np.arange(x2min-0.5*dx2,x2max+dx2, dx2) 
 
     return x1f, x1c, x2f, x2c 
-
 
 # \func fft()
 # Given a 2D array (\phi, R), this computes 
@@ -129,38 +142,105 @@ def ifft(data):
     return fdata 
 
 
+#\func initialize()
+# Initializes everything to be passed to the solver 
+def initialize(prob, zfunc, log):
+    # first setup grid data based on prob  
+    if prob == 'constant':
+        x1min = 0.4
+        x1max = 2.5
+        nx1   = 128
+        x2min = 0.0
+        x2max = 2.0*np.pi
+        nx2   = 128
+    elif prob == 'exp':
+        x1min = 0.4
+        x1max = 2.5
+        nx1   = 128
+        x2min = 0.0
+        x2max = 2.0*np.pi
+        nx2   = 128
+    else:
+        print('[initialize]: prob %s not understood, exiting...' %(prob)) 
 
-def main():
-    nx1 = 512
-    nx2 = nx1
+    # Initialize the grid
+    x1f, x1c, x2f, x2c = init_grid(x1min,x1max,
+                                   x2min,x2max, nx1, nx2, log=log) 
+    # Choose the Gfunc
+    Gfunc = get_Gfunc(zfunc)
 
-    x1f, x1c, x2f, x2c = init_grid(1.,20.,nx1,nx2)
-    Gfunc = get_Gfunc('delta')  
+    # Initialize the grid data 
+    data  = init_data(prob,x1f,x2f) 
+    
+    # Set \phi - \phi'
+    pmpp = np.zeros(len(x1c)) 
 
-    pmpp  = x2c-x2f
-    RP, PMPP, R = np.meshgrid(x1c,pmpp,x1f)  
-
-    # Get Idata 
+    # Initialize I(R,R',\phi-\phi') 
+    RP, PMPP, R = np.meshgrid(x1c, pmpp, x1f) 
     Idata = Ifunc(Gfunc, R, RP, PMPP) 
-    # Get data 
-    data = init_data('constant', nx1, nx2) 
-        
+
+    # Package the grid data 
+    gdata = (x1f, x1c, x2f, x2c) 
+
+    return gdata, data, Idata  
+
+#\func solve()
+# Given the returned stuff from initialize
+# solves for the potential \PHI(R,\phi)
+def solve(myinit):
+    # Parse mystuff
+    gdata, data, Idata = myinit 
+
+    # Parse gdata 
+    x1f, x1c, x2f, x2c = gdata 
+    
     # Take fourier transform of data and Idata 
     fdata  = fft(data)
     fIdata = fft(Idata) 
 
     # Now compute potential in Fourier space 
+    nx2  = len(x2f)-1 
+    nx1  = len(x1f) 
     Phim = np.zeros( (nx2, nx1), dtype='complex') 
     dr   = x1f[1]-x1f[0]
     for j in range(nx2):
         for i in range(nx1):
             # here the last axis is the rp axis (the axis of integration)  
-            Phim[j,i] = trapz(fdata[j,:] * fIdata[j,i,:], dx = (x1f[1]-x1f[0]) )
+            Phim[j,i] = trapz(fdata[j,:] * fIdata[j,i,:], dx = dr)
               
     # Now transfer the potential back to real space
-    # Note that Phi is defined at cell walls 
+        # Phi is defined at the walls of r, but the
+        # centers of \phi 
     Phi = ifft(Phim) 
 
+    return Phi 
+    
+
+
+def main():
+    # Step 0) Read in system arguments 
+    parser = argparse.ArgumentParser(formatter_class=RawTextHelpFormatter)
+
+    parser.add_argument('--prob',type=str,default='constant',
+                        help="Test problem to solve, options are:\n"
+                             "      (a) constant (b) exp",required=False)
+    parser.add_argument('--zfunc',type=str,default='delta',
+                        help="Z(r,z) profile to use, options are:\n"
+                             "      (a) delta (b) gauss",required=False)
+    parser.add_argument('--log',action='store_true',required=False,
+                        help="Switch for log-grid") 
+    args  = parser.parse_args()
+    prob  = args.prob
+    zfunc = args.zfunc 
+    log   = args.log 
+
+    # Step 1) Initialize the system
+    pass_to_solve = initialize(prob,zfunc,log) 
+    Phi           = solve(pass_to_solve) 
+
+
+
+    ''' 
     # Now plot the resultant potential  
     fig = plt.figure()
     ax1 = fig.add_subplot(111) 
@@ -178,6 +258,7 @@ def main():
     plt.figure()
     plt.semilogy(np.abs(gr)) 
     plt.show() 
+    '''
      
 
 
